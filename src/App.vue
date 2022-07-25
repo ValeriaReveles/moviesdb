1c2e0e48db356c3be74f462549eeb5519134f2d1
Brian Carnes <carnes.brian.a@gmail.com>
2022-07-25 16:49:38 -0500

    deleted helloWorld.vue from boilerplate. Added Posts.vue component for fetching.

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
   <router-view/>
 </template>
 
+<script>
+
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="alertButton">Click Me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup () {
+	function alertButton() {
+		console.log("You clicked me")
+	  }
+	  return { alertButton }
   }
 }
 </script>
+
+<style scoped>
+
+</style>
