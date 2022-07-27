0bedd338083ec115c4fe38b72660087f3cb34338
Valeria <valeriareveles12@gmail.com>
2022-07-27 16:31:41 -0500

    finished loading page and styling

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
     component: HomeView
   },
@@ -16,7 +16,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
-    path: '/LoadingPage',
+    path: '/',
     name: 'LoadingPage',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
